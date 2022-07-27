e5648bbdd73fe0204ed939ff768745fefbf34fb3
pooja <pooja@DESKTOP-3P7GERF>
2022-07-27 13:08:43 +0530

    Synchronizationdemo

---
+++ M20_Tns/src/com/tns/core/SynchronizationDemo.java
@@ -0,0 +1,22 @@
+//program on Synchronized
+package com.tns.core;
+
+class Counter
+{
+	int count;
+	public synchronized void inc()
+	{
+		count ++;
+	}
+}
+public class SynchronizationDemo {
+
+	public static void main(String[] args) {
+		Counter c = new Counter();
+		c.inc();
+		System.out.println(c.count);
+
+
+	}
+
+}
